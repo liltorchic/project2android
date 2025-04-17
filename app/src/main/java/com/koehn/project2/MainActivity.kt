0cac0ed534a8ad28ced859839e575ae4e3dae35d
Alex Koehn <mysteryman208@gmail.com>
2025-04-17 18:00:29 -0400

    i dunno it works and looks alrights
    coundnt get theming to work and my project imploded itself on my laptop so idc

--- app/src/main/java/com/koehn/project2/MainActivity.kt
+++ app/src/main/java/com/koehn/project2/MainActivity.kt
@@ -6,12 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.BorderStroke
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -20,30 +23,43 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
+import com.example.compose.AppTheme
 import com.koehn.project2.data.Tip
 import com.koehn.project2.data.tips
-import com.koehn.project2.ui.theme.Project2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Project2Theme {
+            AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize()
@@ -91,9 +107,15 @@ fun TipsTopAppBar(modifier: Modifier = Modifier) {
 
                     contentDescription = null
                 )
+                val offset = Offset(5.0f, 8.0f)
                 Text(
                     text = stringResource(R.string.app_name),
-                    style = MaterialTheme.typography.displayLarge
+                    style = TextStyle(
+                        fontSize = 64.sp,
+                        shadow = Shadow(
+                            color = Color.Black, offset = offset, blurRadius = 8f
+                        )
+                    )
                 )
             }
         },
@@ -128,12 +150,29 @@ fun TipPicture(
     @DrawableRes tipPic: Int,
     modifier: Modifier = Modifier
 ) {
+    val rainbowColorsBrush = remember {
+        Brush.sweepGradient(
+            listOf(
+                Color(0xFF9575CD),
+                Color(0xFFBA68C8),
+                Color(0xFFE57373),
+                Color(0xFFFFB74D),
+                Color(0xFFFFF176),
+                Color(0xFFAED581),
+                Color(0xFF4DD0E1),
+                Color(0xFF9575CD)
+            )
+        )
+    }
+
     Image(
         modifier = modifier
-            .size(dimensionResource(R.dimen.image_size))
+            .fillMaxWidth()
+            .size(dimensionResource(R.dimen.img_size))
             .padding(dimensionResource(R.dimen.padding_small))
-            .clip(MaterialTheme.shapes.small),
-        contentScale = ContentScale.Fit,
+            .clip(MaterialTheme.shapes.small)
+            .border(BorderStroke(4.dp, rainbowColorsBrush)),
+        contentScale = ContentScale.Crop,
         painter = painterResource(tipPic),
 
         // Content Description is not needed here - image is decorative, and setting a null content
@@ -149,27 +188,32 @@ fun TipText(
 ) {
         Text(
             text = stringResource(textID),
-            style = MaterialTheme.typography.bodyMedium
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center,
+            style = LocalTextStyle.current.merge(
+                TextStyle(
+                    lineHeight = 1.25.em,
+                    platformStyle = PlatformTextStyle(
+                        includeFontPadding = false
+                    ),
+                    lineHeightStyle = LineHeightStyle(
+                        alignment = LineHeightStyle.Alignment.Center,
+                        trim = LineHeightStyle.Trim.None
+                    )
+                )
+            ),
+            modifier = Modifier
+                .fillMaxWidth(),
+
         )
-
 }
 
 
-/**
- * Composable that displays what the UI of the app looks like in light theme in the design tab.
- */
-@Preview
-@Composable
-fun LightThemePreview() {
-    Project2Theme(darkTheme = false) {
-        TipsApp()
-    }
-}
 
 @Preview
 @Composable
 fun DarkThemePreview() {
-    Project2Theme(darkTheme = true) {
+    AppTheme() {
         TipsApp()
     }
 }
